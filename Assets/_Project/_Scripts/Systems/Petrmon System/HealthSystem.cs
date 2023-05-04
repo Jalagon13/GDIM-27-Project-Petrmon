7aa7aec0e2bf1f01a5c63687c03b47a7a4f02687
worstdog <50923937+worstdog@users.noreply.github.com>
2023-05-04 14:35:01 -0700

    Update HealthSystem.cs

--- Assets/_Project/_Scripts/Systems/Petrmon System/HealthSystem.cs
+++ Assets/_Project/_Scripts/Systems/Petrmon System/HealthSystem.cs
@@ -9,17 +9,21 @@ namespace ProjectPetrmon
         //private readonly Action _onFaint;
         private readonly int _maxHp;
         private int _currentHp;
+        private int _Defense;
+        private Type _type;
 
         public int MaxHp { get { return _maxHp; } }
         public int CurrentHp { get { return _currentHp; } }
-
-        public HealthSystem(int maxHp/*, Action onFaint*/)
+        public int Defense{ get { return _Defense; } }
+        public HealthSystem(int maxHp, int Defense, Type type/*, Action onFaint*/)
         {
             //_onFaint = onFaint;
             //Fainted += _onFaint;
 
             _maxHp = maxHp;
             _currentHp = maxHp;
+            _Defense = Defense;
+            // _type = type;
         }
 
         // Restores Petrmon HP by the given amount, maxes at _maxHp.
@@ -38,7 +42,11 @@ namespace ProjectPetrmon
 
         public void TakeDamage(int damage)
         {
-            _currentHp -= damage;
+            // if(_type == Type Fire)
+            // {
+            //     _currentHp -= 200;
+            // }
+            _currentHp -= (damage-_Defense);
 
             if(_currentHp <= 0)
             {
