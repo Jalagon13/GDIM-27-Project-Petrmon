e1955e140bb4bcbf08d3503bfc473b9136add35d
Jalagon13 <A1ag0nJus1in@gmail.com>
2023-05-06 19:15:22 -0700

    Attack Move works and stats are ready for game designers

--- Assets/_Project/_Scripts/Systems/Move System/AttackMove.cs
+++ Assets/_Project/_Scripts/Systems/Move System/AttackMove.cs
@@ -9,7 +9,13 @@ namespace ProjectPetrmon
 
         public sealed override void Execute(PetrmonObject fromPetrmon, PetrmonObject toPetrmon)
         {
-            
+            toPetrmon.CurrentHP -= CalculateDamage(fromPetrmon, toPetrmon);
+        }
+
+        private int CalculateDamage(PetrmonObject fromPetrmon, PetrmonObject toPetrmon)
+        {
+            int tempLevel = 1; // replace with fromPetrmon's actual level once we have that coded
+            return (((2 * tempLevel / 5) + 2) * _power * (fromPetrmon.BattleStats.BattleAttack / toPetrmon.BattleStats.BattleDefense)) / 50 + 2;
         }
     }
 }
