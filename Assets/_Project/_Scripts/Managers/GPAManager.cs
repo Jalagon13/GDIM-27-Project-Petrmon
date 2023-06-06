e822d6aa3db1fdec37d14f00991c613e24a6e844
Jalagon13 <A1ag0nJus1in@gmail.com>
2023-06-05 17:11:00 -0700

    GPA functionality works

--- Assets/_Project/_Scripts/Managers/GPAManager.cs
+++ Assets/_Project/_Scripts/Managers/GPAManager.cs
@@ -1,11 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace ProjectPetrmon
 {
     public class GPAManager : Singleton<GPAManager>
     {
-        
+        [SerializeField] private TextMeshProUGUI _gpaText;
+
+        private readonly float _startingGPA = 1.1f;
+        private float _currentGPA;
+
+        private void Start()
+        {
+            AwardGPA(_startingGPA);
+        }
+
+        public void AwardGPA(float gpaAwarded)
+        {
+            _currentGPA += gpaAwarded;
+            UpdateGPAText();
+        }
+
+        private void UpdateGPAText()
+        {
+            _gpaText.text = $"CURRENT GPA: {_currentGPA}";
+        }
     }
 }
